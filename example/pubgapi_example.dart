f9bb63d33fc2bf897ff3a1aa12ed020ac7da3308
Panic <panic@panics-mbp.lan>
2018-05-03 03:53:50 -0600

    added license and updated example

--- example/pubgapi_example.dart
+++ example/pubgapi_example.dart
@@ -1,6 +1,11 @@
-// import 'package:pubgapi/pubgapi.dart';
+import 'package:pubgapi/pubgapi.dart';
 
-// main() {
-//   var awesome = new Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+main() {
+  var api = PUBGAPI("123131afsafsa");
+  
+    api.getPlayerByName("PanicIsReal")
+    .then((player) {
+      print(player.data[0].attributes.name);
+    })
+    .catchError((e) => print(e));
+}
