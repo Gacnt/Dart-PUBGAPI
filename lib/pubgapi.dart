7de5e4bcbe60b6bfcc4d38cd87c6d2c08bcec65e
Panic <panic@panics-mbp.lan>
2018-05-15 01:49:41 -0600

    Exported classes for data

--- lib/pubgapi.dart
+++ lib/pubgapi.dart
@@ -4,5 +4,10 @@
 library pubgapi;
 
 export 'src/pubgapi_base.dart';
+export 'src/models/match.dart';
+export 'src/models/player.dart';
+export 'src/models/season.dart';
+export 'src/models/telemetry.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
